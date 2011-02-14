9127216d22764c1ab6008616f7d8db3e30d41c14
Apen <cerdanyohann@yahoo.fr>
2011-02-14 09:51:42 +0100

    Initial upload

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "adgallery".
+ *
+ * Auto generated 06-12-2015 16:29
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Multimedia gallery based on adgallery',
+	'description' => 'Multimedia gallery based on adgallery',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '4.3.0-0.0.0',
+	'PHP_version' => '5.0.0-0.0.0',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'CERDAN Yohann',
+	'author_email' => 'cerdanyohann@yahoo.fr',
+	'author_company' => 'Site\'nGO',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+);
+
+?>
